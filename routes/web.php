4d70690cb731a16f2f6f955bf21aaf794ebbed7e
Hamedi-Saboor <saboorhamedi52@gmail.com>
2025-03-25 11:50:02 +0700

    After react and inertia

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Livewire\Actions\Logout;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+Route::get('/', function () {
+    return view('welcome');
+});
